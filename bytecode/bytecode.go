a107b27ac2626a356de10cde98f8162ba224d52b
Արմեն Բադալյան <armenbadal@gmail.com>
2025-07-18 18:21:43 +0400

    Հրամանների կոդերի ու անունների արտապատկերում

--- bytecode/bytecode.go
+++ bytecode/bytecode.go
@@ -28,6 +28,62 @@ const (
 	Ge
 )
 
+var Codes = []byte{
+	Nop,
+	Push,
+	Pop,
+	Call,
+	Ret,
+	Jump,
+	Jz,
+	Halt,
+	Input,
+	Print,
+	Add,
+	Sub,
+	Mul,
+	Div,
+	Mod,
+	Neg,
+	And,
+	Or,
+	Not,
+	Eq,
+	Ne,
+	Lt,
+	Le,
+	Gt,
+	Ge,
+}
+
+var Mnemonics = map[byte]string{
+	Nop:   "NOP",
+	Push:  "PUSH",
+	Pop:   "POP",
+	Call:  "CALL",
+	Ret:   "RET",
+	Jump:  "JUMP",
+	Jz:    "JZ",
+	Halt:  "HALT",
+	Add:   "ADD",
+	Sub:   "SUB",
+	Mul:   "MUL",
+	Div:   "DIV",
+	Mod:   "MOD",
+	Neg:   "NEG",
+	And:   "AND",
+	Or:    "OR",
+	Not:   "NOT",
+	Eq:    "EQ",
+	Ne:    "NE",
+	Lt:    "LT",
+	Le:    "LE",
+	Gt:    "GT",
+	Ge:    "GE",
+	Input: "INPUT",
+	Print: "PRINT",
+}
+
 const (
 	Basic     byte = 0x00
 	Immediate byte = 0x40
